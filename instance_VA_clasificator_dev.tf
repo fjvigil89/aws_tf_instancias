22593477fee9a209f5ef0248f64756bd16116261
Frank Josué Vigil Vega <frankjosue.vigilvega@gmail.com>
2022-05-11 19:52:07 +0200

    Gregando Instancias DEV & PROD by Vision Artify

---
+++ instance_VA_clasificator_dev.tf
@@ -0,0 +1,101 @@
+# Elastic IPS
+resource "aws_eip" "VA_clasificator_dev" {
+  vpc = true
+
+  tags = {
+    Name     = "IP elastica"
+    Episodio = "Vision Artificial"
+  }
+
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+resource "aws_efs_file_system" "VA_clasificator_dev" {
+  creation_token   = "VA_clasificator_dev"
+  encrypted        = true
+  performance_mode = "generalPurpose"
+
+  tags = {
+    Name     = "EFS"
+    Episodio = "Vision Artificial"
+  }
+}
+
+resource "aws_efs_mount_target" "VA_clasificator_dev" {
+  count           = length(data.aws_availability_zones.available.zone_ids)
+  file_system_id  = aws_efs_file_system.VA_clasificator_dev.id
+  subnet_id       = element(aws_subnet.privada.*.id, count.index)
+  security_groups = [aws_security_group.efs.id]
+}
+
+
+resource "aws_instance" "VA_clasificator_dev" {
+  count = 1
+  #availability_zone      = "eu-west-1b"
+  ami                    = "ami-0d527b8c289b4af7f" // AMI son regionales (distintas IDS por region) instancia de ubuntu en la region de irlandia
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.public[1].id
+  vpc_security_group_ids = concat([aws_security_group.servidor_web.id], [aws_security_group.efs.id], [aws_default_security_group.default.id])
+  key_name               = aws_key_pair.laptop.id
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  root_block_device {
+    volume_type           = "gp2"
+    volume_size           = "10"
+    delete_on_termination = true
+  }
+
+  user_data = <<-EOF
+                #!/bin/bash
+                # ---> Updating, upgrating and installing the base
+                apt update
+                apt install python3-pip apt-transport-https ca-certificates curl software-properties-common nfs-common -y
+                mkdir /var/lib/docker
+                echo "${aws_efs_file_system.VA_clasificator_dev.dns_name}:/  /var/lib/docker    nfs4   nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 0 2" >> /etc/fstab
+                mount -a
+                curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+                add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+                apt update && apt upgrade -y
+                apt install docker-ce -y
+                systemctl status docker
+                usermod -aG docker ubuntu
+                #--docker run -p 80:80 -d nginxdemos/hello
+                pip install -r requirements.txt
+                EOF
+
+  tags = {
+    Name     = "EC2 con persistencia en ${aws_subnet.public[count.index].availability_zone}"
+    Episodio = "Vision Artificial"
+  }
+
+  depends_on = [aws_efs_file_system.VA_clasificator_dev, aws_efs_mount_target.VA_clasificator_dev]
+}
+
+resource "aws_eip_association" "VA_clasificator_dev" {
+  instance_id   = aws_instance.VA_clasificator_dev[0].id
+  allocation_id = aws_eip.VA_clasificator_dev.id
+}
+
+output "VA_clasificator_dev" {
+  value = aws_eip.VA_clasificator_dev.public_dns
+}
+
+resource "null_resource" "clasificator_dev" {
+  provisioner "file" {
+    source      = "script/requirements.txt"
+    destination = "/tmp/requirements.txt"
+  }
+  connection {
+    type        = "ssh"
+    host        = aws_eip.VA_clasificator_dev.public_dns
+    user        = "ubuntu"
+    private_key = file(var.ssh_priv_path)
+  }
+
+  depends_on = [aws_eip_association.VA_clasificator_dev]
+}
